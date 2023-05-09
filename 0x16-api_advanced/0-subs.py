299687d6e33e22c311342b45cfd692be6e8ae179
Lukman Asinmi <eemandigitalconcept@gmail.com>
2023-05-09 22:25:52 +0000

    error fixed

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,16 +3,11 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """ query Reddit API and return number of subscribers
-    for given subreddit
-    """
-    url = "http://www.reddit.com/r/{:s}/about.json".format(subreddit)
-    headers = {'user-agent': 'egsyquest'}
-    r = requests.get(url, headers=headers)
-
-    if (r.status_code is 302):
-        return 0
-    if (r.status_code is 404):
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+    url = 'https://www.reddit.com/r/%s/about.json' % subreddit    
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        return response.json()['data']['subscribers']
+    else:
         return 0
 
-    return r.json()['data'].get('subscribers', 0)
